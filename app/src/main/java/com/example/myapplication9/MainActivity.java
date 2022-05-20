baef6fe57112da6ec9ff5c9a5bbc20809b43d66e
IL09 <Siddharth.Pilli@infinitylearn.com>
2022-05-20 16:10:14 +0530

    Removed API References & Unusable Code

--- app/src/main/java/com/example/myapplication9/MainActivity.java
+++ app/src/main/java/com/example/myapplication9/MainActivity.java
@@ -44,9 +44,8 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText spectre_text, fsi_text;
-    private Spinner spinner, zone_spinner, ground_spinner,  importance_spinner, site_spinner,loss_spinner;
-    private CheckBox ch1,ch2,ch3,ch4,ch5,ch6,ch7,ch8,ch9,ch10,ch11,ch12,ch13,ch14,ch15,ch16;
+    EditText spectre_text, fsi_text;
+    CheckBox ch1,ch2,ch3,ch4,ch5,ch6,ch7,ch8,ch9,ch10,ch11,ch12,ch13,ch14,ch15,ch16,ch17;
 
     TextView coltextView;
     boolean[] selectedCol;
@@ -307,7 +306,7 @@ public class MainActivity extends AppCompatActivity {
         ch14 = (CheckBox)findViewById(R.id.lossSite1);
         ch15 = (CheckBox)findViewById(R.id.lossSoil1);
         ch16 = (CheckBox)findViewById(R.id.lossSoil2);
-
+        ch17 = (CheckBox)findViewById(R.id.suit_soil1);
 
 //        System.out.println(ch1_val);
 
@@ -329,158 +328,8 @@ public class MainActivity extends AppCompatActivity {
 //                System.out.println(ch5_val);
 //                System.out.println(ch6_val);
 
-                saveData(createRequest());
+//                saveData(createRequest());
 
             }
         });
-    }
-    public UserRequest createRequest(){
-        UserRequest userRequest = new UserRequest();
-
-
-//        if(ch1.isChecked())
-//            ch1_val = "True";
-//        if(ch2.isChecked())
-//            ch2_val = "True";
-//        if(ch3.isChecked())
-//            ch3_val = "True";
-//        if(ch4.isChecked())
-//            ch4_val = "True";
-//        if(ch5.isChecked())
-//            ch5_val = "True";
-//        if(ch6.isChecked())
-//            ch6_val = "True";
-//        if(ch7.isChecked())
-//            ch7_val = "True";
-//        if(ch8.isChecked())
-//            ch8_val = "True";
-//        if(ch9.isChecked())
-//            ch9_val = "True";
-//        if(ch10.isChecked())
-//            ch10_val = "True";
-//        if(ch11.isChecked())
-//            ch11_val = "True";
-//        if(ch12.isChecked())
-//            ch12_val = "True";
-//        if(ch13.isChecked())
-//            ch13_val = "True";
-//        if(ch14.isChecked())
-//            ch14_val = "True";
-//        if(ch15.isChecked())
-//            ch15_val = "True";
-//        if(ch16.isChecked())
-//            ch16_val = "True";
-
-        userRequest.setCollateral_damage(col_data);
-        userRequest.setZone_factor(zone_data);
-        userRequest.setGround_shaking(soil_data);
-        userRequest.setImportance_i(imp_data);
-        userRequest.setSpectre_shape(spectre_text.getText().toString());
-        userRequest.setFsi(fsi_text.getText().toString());
-        userRequest.setSite1(ch1.isChecked());
-        userRequest.setSite2(ch2.isChecked());
-        userRequest.setSite3(ch3.isChecked());
-        userRequest.setSoil1(ch4.isChecked());
-        userRequest.setSoil2(ch5.isChecked());
-        userRequest.setArch1(ch6.isChecked());
-        userRequest.setArch2(ch7.isChecked());
-        userRequest.setStrc1(ch8.isChecked());
-        userRequest.setStrc2(ch9.isChecked());
-        userRequest.setStrc3(ch10.isChecked());
-        userRequest.setCon1(ch11.isChecked());
-        userRequest.setCon2(ch12.isChecked());
-        userRequest.setCon3(ch13.isChecked());
-        userRequest.setLossSite1(ch14.isChecked());
-        userRequest.setLossSoil1(ch15.isChecked());
-        userRequest.setLossSoil2(ch16.isChecked());
-//        userRequest.setSpectre_shape(spectre_text);
-
-        return userRequest;
-    }
-
-    public void saveData(UserRequest userRequest){
-
-        Call<UserResponse> userResponseCall = AppClient.getUserService().saveUsers(userRequest);
-        userResponseCall.enqueue(new Callback<UserResponse>() {
-            @Override
-            public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
-                System.out.println(response.code());
-                System.out.println(response.body());
-
-                if(response.isSuccessful()){
-                    Toast.makeText(MainActivity.this, "Saved Succssfully",Toast.LENGTH_LONG).show();
-                }else{
-                    Toast.makeText(MainActivity.this, response.code(),Toast.LENGTH_LONG).show();
-                }
-
-            }
-
-            @Override
-            public void onFailure(Call<UserResponse> call, Throwable t) {
-//                Toast.makeText(MainActivity.this, "Request Failed "+t.getLocalizedMessage(),Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-}
-//    private void postDataUsingVolley(String name) {
-//        // url to post our data
-//        String url = "https://webhook.site/6a1a0260-39a7-4eb8-a071-0857480a1bfd";
-//
-//        // creating a new variable for our request queue
-//        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-//
-//        // on below line we are calling a string
-//        // request method to post the data to our API
-//        // in this we are calling a post method.
-//        StringRequest request = new StringRequest(Request.Method.POST, url, new com.android.volley.Response.Listener<String>() {
-//            @Override
-//            public void onResponse(String response) {
-//                // inside on response method we are
-//                // hiding our progress bar
-//                // and setting data to edit text as empty
-//
-//
-//                // on below line we are displaying a success toast message.
-//                Toast.makeText(MainActivity.this, "Data added to API", Toast.LENGTH_SHORT).show();
-//                try {
-//                    // on below line we are parsing the response
-//                    // to json object to extract data from it.
-//                    JSONObject respObj = new JSONObject(response);
-//
-//                    // below are the strings which we
-//                    // extract from our json object.
-//                    String name = respObj.getString("name");
-//
-//                    // on below line we are setting this string s to our text view.
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }, new com.android.volley.Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                // method to handle errors.
-//                Toast.makeText(MainActivity.this, "Fail to get response = " + error, Toast.LENGTH_SHORT).show();
-//            }
-//        }) {
-//            @Override
-//            protected Map<String, String> getParams() {
-//                // below line we are creating a map for
-//                // storing our values in key and value pair.
-//                Map<String, String> params = new HashMap<String, String>();
-//
-//                // on below line we are passing our key
-//                // and value pair to our parameters.
-//                params.put("name", name);
-//
-//                // at last we are
-//                // returning our params.
-//                return params;
-//            }
-//        };
-//        // below line is to make
-//        // a json object request.
-//        queue.add(request);
-//    }
-//}
-//
+    }}
