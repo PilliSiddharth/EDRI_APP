5491485d82904a49a895dc4dca4c70d0f086b500
k sanketh kumar <k.sanketh123@gmail.com>
2022-06-10 14:37:45 +0530

    Added Firebase Authentication

--- app/src/main/java/com/example/myapplication9/MainActivity.java
+++ app/src/main/java/com/example/myapplication9/MainActivity.java
@@ -38,15 +38,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
-
-import com.github.mikephil.charting.animation.Easing;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.formatter.PercentFormatter;
-import com.github.mikephil.charting.utils.ColorTemplate;
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
